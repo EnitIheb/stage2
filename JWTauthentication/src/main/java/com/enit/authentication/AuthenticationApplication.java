49e9d9663fa6400313034457e6ac818d016fcd51
EnitIheb <iheb.boumaiza@etudiant-enit.utm.tn>
2020-07-26 08:11:40 +0100

    add spring config and it worked dont touch anything and ty

--- JWTauthentication/src/main/java/com/enit/authentication/AuthenticationApplication.java
+++ JWTauthentication/src/main/java/com/enit/authentication/AuthenticationApplication.java
@@ -3,6 +3,8 @@ package com.enit.authentication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class AuthenticationApplication {
 
--- JWTauthentication/src/main/java/com/enit/authentication/controller/AuthRestAPIs.java
+++ JWTauthentication/src/main/java/com/enit/authentication/controller/AuthRestAPIs.java
@@ -18,6 +18,7 @@ import com.enit.authentication.repository.RoleRepository;
 import com.enit.authentication.repository.UserRepository;
 import com.enit.authentication.security.jwt.JwtProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -34,7 +35,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@RefreshScope
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
